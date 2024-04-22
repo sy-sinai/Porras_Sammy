c63f14a5e16ea03e5efe76d78a5e8d8c3aa329c3
laboratorio <laboratorio@UPEAULA-31711>
2024-04-22 07:26:44 -0500

    Agregar archivos de proyecto.

---
+++ Porras_Sammy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Porras_Sammy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
