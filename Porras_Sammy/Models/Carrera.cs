76b5f27f2b50829ec07fa5b116d47fa9433aea0c
sy-sinai <sammy.porras@udla.edu.ec>
2024-04-22 08:02:26 -0500

    .commit

---
+++ Porras_Sammy/Models/Carrera.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Porras_Sammy.Models
+{
+    public class Carrera
+    {
+        [Key]
+        public string? Nnombre_carrera { get; set; }
+        public string? Campus { get; set; }
+        public string? numero_semestres { get; set; }
+        
+    }
+}
---
+++ Porras_Sammy/Models/SPorras.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Porras_Sammy.Models
+{
+    public class SPorras
+    {
+        [Key]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public Boolean EstudianteActivo { get; set; }// El estad del estudiante
+        public required int IDBanner { get; set; } //numero identificador de banner
+        [AllowNull]
+        public float ValorSemestral { get; set; }// valor del semestre 
+        public string? Nombre { get; set; } //Nombre completo del estudiante
+        public DateTime? FechaIngreso { get; set; } // fecha de ingreso a la universidad
+        public Carrera carrera { get; set; }
+       
+        
+    }
+}
